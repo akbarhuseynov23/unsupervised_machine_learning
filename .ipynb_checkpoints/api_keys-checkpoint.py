8ecea7f2e790d825308b15d47d5eb9ba0c404075
Akbar <92020851+akbarhuseynov23@users.noreply.github.com>
2021-12-10 11:08:30 +0100

    files added

---
+++ .ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+OWM_key = "0c87854d706efc6d2ef016fb03918d7a"
+flight_api_key = "e39db7f26bmsha13057ceeef84dbp102050jsn189f13591088"
+spotify_client_id = "4158e516be9846648f4ff58ca3466b32"
+spotify_client_secret = "8b29f47f26bb4b99abcce74e33744c92"
+spotify_username = "akbarhuseynov"
